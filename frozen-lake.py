7663f62bacd9e78d46ee1000248faaf67731901b
Sebastian Seurig <sebastian@seurig.com>
2020-08-29 21:47:24 +0200

    changed frozen lake mvg avg to batch avg

--- frozen-lake.py
+++ frozen-lake.py
@@ -3,31 +3,35 @@
 # agent may slide
 
 # 1000 games
-# plot win% trailing 10 games
+# plot win% for every 10 games
 
 import gym
 import numpy as np
 import matplotlib.pyplot as plt
 
-env = gym.make('FrozenLake-v0')
+env = gym.make('FrozenLake-v0') # https://gym.openai.com/envs/FrozenLake-v0/
 
 wins = []
+avg_win_10_games = []
 
-for _ in range(1000):
+for i in range(1000):
     env.reset()
     done = False
+
     while done is False:
         #env.render()
-        observation, reward, done, _ = env.step(env.action_space.sample()) # take a random action
-    wins.append(reward)
+        action = env.action_space.sample()
+        observation, reward, done, _ = env.step(action) # take a random action
+
+    wins.append(reward) # either 0 or 1 when game is done - no reward given before
+
+    if i % 10 == 0:
+        avg_win_10_games.append(np.mean(wins[-10:]) * 100)
+
 env.close()
 
-def moving_average(arr, window):
-    return np.convolve(arr, np.ones(window), 'valid') / window
-
-wins_trailing_10_games = moving_average(np.array(wins), 10) * 100
-plt.plot(np.arange(len(wins_trailing_10_games)), wins_trailing_10_games)
+plt.plot(np.arange(len(avg_win_10_games)), avg_win_10_games)
 plt.xlabel('batch number')
 plt.ylabel('win rate (%)')
-plt.title('10 game moving average of win rate')
+plt.title('10 game average of win rate')
 plt.savefig('plots/frozen-lake.png')
