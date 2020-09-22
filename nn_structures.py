5d7fb0ff7744a6bfcdd323d4828b016e011835eb
Sebastian Seurig <sebastian@seurig.com>
2020-09-22 19:46:59 +0200

    dddql implemented

--- nn_structures.py
+++ nn_structures.py
@@ -78,3 +78,52 @@ class ConvNeuralNet(nn.Module):
 
     def save_weights(self):
         T.save(self.state_dict(), self.weights_file)
+
+class DuelingDeepNet(nn.Module):
+    def __init__(self, input_dims, output_dims, lr, weights_file):
+        super(DuelingDeepNet, self).__init__()
+        self.weights_file = weights_file
+        
+        self.conv1 = nn.Conv2d(input_dims[0], 32, 8, stride=4)
+        self.conv2 = nn.Conv2d(32, 64, 4, stride=2)
+        self.conv3 = nn.Conv2d(64, 64, 3, stride=1)
+        fc_input_dims = self.calculate_fc_input_dims(input_dims)
+
+        self.fc1 = nn.Linear(fc_input_dims, 512)
+
+        self.V_layer = nn.Linear(512, 1)
+        self.A_layer = nn.Linear(512, output_dims)
+
+        self.loss = nn.MSELoss()
+        self.optimizer = optim.RMSprop(self.parameters(), lr=lr)
+
+        self.device = T.device('cuda:0' if T.cuda.is_available() else 'cpu')
+        self.to(self.device)
+
+        if os.path.isfile(self.weights_file):
+            self.load_weights()
+
+    def calculate_fc_input_dims(self, input_dims):
+        tnsr = T.zeros(1, *input_dims)
+        dims = self.conv1(tnsr)
+        dims = self.conv2(dims)
+        dims = self.conv3(dims)
+        return int(np.prod(dims.size()))
+
+    def forward(self, data):       
+        conv1 = F.relu(self.conv1(data))
+        conv2 = F.relu(self.conv2(conv1))
+        conv3 = F.relu(self.conv3(conv2))
+        flatten = conv3.view(conv3.size()[0], -1)
+
+        fc1 = F.relu(self.fc1(flatten))
+        V = self.V_layer(fc1)
+        A = self.A_layer(fc1)
+
+        return V, A
+
+    def load_weights(self):
+        self.load_state_dict(T.load(self.weights_file))
+
+    def save_weights(self):
+        T.save(self.state_dict(), self.weights_file)
